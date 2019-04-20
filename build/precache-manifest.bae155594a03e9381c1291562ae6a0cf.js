5dd69e377aa277738962b7006ea5fd9df4177da8
Serge W <ssa990@ya.ru>
2019-04-20 23:24:30 +0700

    !

---
+++ build/precache-manifest.bae155594a03e9381c1291562ae6a0cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "985c4d72c300851924a4",
+    "url": "/static/js/main.c477f50c.chunk.js"
+  },
+  {
+    "revision": "e0ff2ce71b1cce65a25b",
+    "url": "/static/js/2.078ab91d.chunk.js"
+  },
+  {
+    "revision": "985c4d72c300851924a4",
+    "url": "/static/css/main.739e1470.chunk.css"
+  },
+  {
+    "revision": "a91e3d752863ce15e9c6650deb32b74d",
+    "url": "/index.html"
+  }
+];
