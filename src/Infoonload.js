bcba3588aca0eea1089a6d59faff64f19cf140f3
Serge W <ssa990@ya.ru>
2019-04-20 17:17:23 +0700

    add Infoonload block

---
+++ src/Infoonload.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class InfoBlockOnLoad extends React.Component {
+  render() {
+    return (
+      <>
+        <div id="infoblock">
+          Для добавления пункта в список дел нажмите кнопку <span>+</span>
+        </div>
+      </>
+    )
+  }
+};
+
+export default InfoBlockOnLoad;
